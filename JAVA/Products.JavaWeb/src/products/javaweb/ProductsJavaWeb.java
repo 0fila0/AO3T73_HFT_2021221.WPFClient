28b6d174c0cfac8a854e0308996edeca15f87d28
0fila0 <filanorbertlaszlo@gmail.com>
2019-10-23 16:12:20 +0200

    Java project has been added.

---
+++ JAVA/Products.JavaWeb/src/products/javaweb/ProductsJavaWeb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package products.javaweb;
+
+/**
+ *
+ * @author Norbert
+ */
+public class ProductsJavaWeb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
