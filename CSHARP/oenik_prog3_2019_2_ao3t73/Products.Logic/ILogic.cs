ffb8bdfaf29959d17194b4dc8c4d54d604e1488b
0fila0 <filanorbertlaszlo@gmail.com>
2019-11-13 19:35:26 +0100

    ILogic interface has been created.

---
+++ CSHARP/oenik_prog3_2019_2_ao3t73/Products.Logic/ILogic.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ILogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Products.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This interface describes what methods should be implemented in Logic class.
+    /// </summary>
+    public interface ILogic
+    {
+    }
+}
