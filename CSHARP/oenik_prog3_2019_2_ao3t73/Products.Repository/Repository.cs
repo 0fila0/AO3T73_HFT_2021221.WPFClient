2517756b191bb4d2c7ba229b908716d9ab40f9f8
0fila0 <filanorbertlaszlo@gmail.com>
2019-11-13 17:50:28 +0100

    'IRepository' has been implemented automatically in 'Repository'.

--- CSHARP/oenik_prog3_2019_2_ao3t73/Products.Repository/Repository.cs
+++ CSHARP/oenik_prog3_2019_2_ao3t73/Products.Repository/Repository.cs
@@ -9,8 +9,108 @@ namespace Products.Repository
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Products.Data;
 
-    public class Repository
+    public class Repository : IRepository
     {
+        public void AddLink(ID_Kapcsolo param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddManufacturer(Gyarto param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProduct(Termek param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddShop(Aruhaz param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteLink(ID_Kapcsolo param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteManufacturer(Gyarto param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Termek param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteShop(Aruhaz param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ID_Kapcsolo FindLink(string key_param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Gyarto FindManufaceturer(string key_param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Termek FindProduct(string key_param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aruhaz FindShop(string key_param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ID_Kapcsolo> GetAllLinks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Gyarto> GetAllManufacturers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Termek> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aruhaz> GetAllShops()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateLink(ID_Kapcsolo param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateManufacturer(Gyarto param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Termek param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateShop(Aruhaz param)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
