31e9dcd5ca5ff3969233e8c8df9b2e01d1691514
0fila0 <filanorbertlaszlo@gmail.com>
2019-12-03 20:42:45 +0100

    'using System.Reflection' has been added to '.Repository'.

--- CSHARP/oenik_prog3_2019_2_ao3t73/Products.Repository/Repository.cs
+++ CSHARP/oenik_prog3_2019_2_ao3t73/Products.Repository/Repository.cs
@@ -7,6 +7,7 @@ namespace Products.Repository
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using System.Text;
     using System.Threading.Tasks;
     using Products.Data;
