4024ae36222c3f076cd5908eac2d508a87506926
0fila0 <filanorbertlaszlo@gmail.com>
2019-12-04 01:53:31 +0100

    DontWriteToTheStandardOutput and ThisIsAnID attributes has been created and implemented into the .Data class.

---
+++ CSHARP/oenik_prog3_2019_2_ao3t73/Products.Data/DontWriteToTheStandardOutput.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="DontWriteToTheStandardOutput.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Products.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This attribute prevents from write some unusable property to the standard output.
+    /// </summary>
+    public class DontWriteToTheStandardOutput : Attribute
+    {
+    }
+}
---
+++ CSHARP/oenik_prog3_2019_2_ao3t73/Products.Data/ThisIsAnID.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ThisIsAnID.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Products.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This attribute shows which property is the entity's ID.
+    /// </summary>
+    public class ThisIsAnID : Attribute
+    {
+    }
+}
