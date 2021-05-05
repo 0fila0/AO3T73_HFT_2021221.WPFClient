2c027b22a935529da90b922e089a1b1a812ce287
0fila0 <filanorbertlaszlo@gmail.com>
2021-05-05 11:44:39 +0200

    Warnings handled.

--- AruhazWeb/Controllers/HomeController.cs
+++ AruhazWeb/Controllers/HomeController.cs
@@ -1,37 +1,60 @@
-﻿using AruhazWeb.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// <copyright file="HomeController.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace AruhazWeb.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AruhazWeb.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Home controller.
+    /// </summary>
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="logger"> Logger. </param>
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            this._logger = logger;
         }
 
+        /// <summary>
+        /// Index method.
+        /// </summary>
+        /// <returns> A new view. </returns>
         public IActionResult Index()
         {
-            return View();
+            return this.View();
         }
 
+        /// <summary>
+        /// Privacy method.
+        /// </summary>
+        /// <returns> A new view. </returns>
         public IActionResult Privacy()
         {
-            return View();
+            return this.View();
         }
 
+        /// <summary>
+        /// Error method.
+        /// </summary>
+        /// <returns> A new view. </returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
